99123b015f8ae028e263bd0d3c4f9184d66a24d3
emmaalecrim <emmaalecrim@gmail.com>
2024-05-02 13:21:00 +0200

    finished exercici2

--- src/s1/t1/n1/exercici2/Main.java
+++ src/s1/t1/n1/exercici2/Main.java
@@ -1,16 +1,37 @@
 package javaPlayground.src.s1.t1.n1.exercici2;
 
 public class Main {
-    
+    void main(String[] args) {
+        Cotxe car = new Cotxe("Camaro"); 
+        System.out.println("Su potencia es:" + car.potencia);
+
+        car.accelerar();
+        Cotxe.frenar();
+    }
+        
 }
 
 class Cotxe {
     static final String marca = "Ford";
     static String model = "Mustang";
-    final int potencia = 250;
+    final int potencia = 250;       
 
-    Cotxe() {
-        
+    static {
+        System.out.printf("Base cotxe cargado en memoria. Defaults: Marca: %s, Model: %s\n", Cotxe.marca, Cotxe.model);
     }
-       
+
+    Cotxe(String model){
+        Cotxe.model = model;
+        System.out.println("Model: " + Cotxe.model);
+    }
+
+
+    static void frenar() {
+        System.out.println("El vehicle està frenant");
+    }
+
+    void accelerar() {
+        System.out.println("El vehicle està accelerant");
+    }
+
 }
