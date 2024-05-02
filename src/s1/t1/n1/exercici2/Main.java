0d739936b4982945ee2fe7735aca15878f9e9074
emmaalecrim <emmaalecrim@gmail.com>
2024-05-02 13:05:54 +0200

    started exercici2

---
+++ src/s1/t1/n1/exercici2/Main.java
@@ -0,0 +1,16 @@
+package javaPlayground.src.s1.t1.n1.exercici2;
+
+public class Main {
+    
+}
+
+class Cotxe {
+    static final String marca = "Ford";
+    static String model = "Mustang";
+    final int potencia = 250;
+
+    Cotxe() {
+        
+    }
+       
+}
