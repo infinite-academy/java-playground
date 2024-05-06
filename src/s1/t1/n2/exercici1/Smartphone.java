472be0e0c971d9d1018f7c4dfaa9c4b7a4886563
emmaalecrim <emmaalecrim@gmail.com>
2024-05-06 10:50:15 +0200

    extracted functional classes into their own packages

---
+++ src/s1/t1/n2/exercici1/Smartphone.java
@@ -0,0 +1,19 @@
+package s1.t1.n2.exercici1;
+
+class Smartphone extends Telefon implements Types, Rellotge {
+
+    Smartphone(String marca, String model){
+        super(marca, model);
+    }
+
+
+    @Override
+    public void fotografiar() {
+        System.out.println("S'està fent una foto");
+    }
+
+    @Override
+    public void alarma() {
+        System.out.println("Està sonant l'alarma");
+    }
+}
