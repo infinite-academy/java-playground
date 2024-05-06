12ec1daf6aa59dadbc7aaf5fa9375e00486963e9
emmaalecrim <emmaalecrim@gmail.com>
2024-05-06 10:43:32 +0200

    extracted program logic into Main

--- src/s1/t1/n1/exercici1/InstrumentTest.java
+++ src/s1/t1/n1/exercici1/InstrumentTest.java
@@ -1,23 +1,5 @@
 package s1.t1.n1.exercici1;
 
-import java.util.Random;
-
-
-
-public class InstrumentTest {
-    public static void main(String[] args){
-        System.out.println("Assert that instrument is loaded via static accessor(loaded) before instantiating: " + Instrument.loaded);
-
-        VentInstrument vent = new VentInstrument("Flauta", (new Random().nextFloat() + 1) * 1000);        
-        vent.tocar();
-
-        CordaInstrument corda = new CordaInstrument("Guitar", (new Random().nextFloat() + 1) * 1000);        
-        corda.tocar();
-
-        PercusioInstrument percusio = new PercusioInstrument("Tamborin", (new Random().nextFloat() + 1) * 1000);
-        percusio.tocar();
-    }
-}
 
 // If i use this interface it breaks the code. Why? 
 // interface Instrument{
---
+++ src/s1/t1/n1/exercici1/Main.java
@@ -0,0 +1,20 @@
+package s1.t1.n1.exercici1;
+
+import java.util.Random;
+
+
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Assert that instrument is loaded via static accessor(loaded) before instantiating: " + Instrument.loaded);
+
+        VentInstrument vent = new VentInstrument("Flauta", (new Random().nextFloat() + 1) * 1000);        
+        vent.tocar();
+
+        CordaInstrument corda = new CordaInstrument("Guitar", (new Random().nextFloat() + 1) * 1000);        
+        corda.tocar();
+
+        PercusioInstrument percusio = new PercusioInstrument("Tamborin", (new Random().nextFloat() + 1) * 1000);
+        percusio.tocar();
+    }
+}
