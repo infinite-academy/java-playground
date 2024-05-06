23b9584de0012271100c5df2f9405dc64132f96f
emmaalecrim <emmaalecrim@gmail.com>
2024-05-06 10:50:15 +0200

    extracted functional classes into their own packages

---
+++ src/s1/t1/n2/exercici1/Types.java
@@ -0,0 +1,9 @@
+package s1.t1.n2.exercici1;
+
+interface Types {
+    void fotografiar();
+}
+
+interface Rellotge {
+    void alarma();
+}
--- src/s1/t1/n3/exercici1/Noticia.java
+++ src/s1/t1/n3/exercici1/Noticia.java
@@ -1,9 +1,5 @@
 package s1.t1.n3.exercici1;
 
-public class ManejaRedacio {
-    
-}
-
 interface NoticiaDef {
     void calcularPreuNoticia(String[] args);
 }
@@ -35,19 +31,3 @@ class Noticia implements NoticiaDef {
 
     // void calcularPreuNoticia()
 }
-
-class Redactor {
-    String nom = "";
-    // Note: To assign final fields at construction time, do not assign at time of init.
-    final String dni;
-    static float sou = 1500.0f;
-    final Noticia[] Noticias = {};
-
-    Redactor(String nom, String dni, float sou) {
-        this.nom = nom;
-        this.dni = dni;
-    }
-
-}
-
-// TODO: Refactor t1 to use least privilege access
