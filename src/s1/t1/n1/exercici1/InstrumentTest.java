7bf3d5c701e5c3da584a05c29ae6ce4240b9c581
emmaalecrim <emmaalecrim@gmail.com>
2024-05-06 10:15:15 +0200

    new branch for review

--- src/s1/t1/n1/exercici1/InstrumentTest.java
+++ src/s1/t1/n1/exercici1/InstrumentTest.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 
 
-public class Main {
+public class InstrumentTest {
     public static void main(String[] args){
         System.out.println("Assert that instrument is loaded via static accessor(loaded) before instantiating: " + Instrument.loaded);
         for (Instrument.Tipus tipus : Instrument.Tipus.values()) {
