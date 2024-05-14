f8f1a4ae7147f3b4d13cb7669fd336bb64a76f59
emmaalecrim <emmaalecrim@gmail.com>
2024-05-14 18:39:59 +0200

    extracted classes

---
+++ src/s1/t1/n1/exercici1/CordaInstrument.java
@@ -0,0 +1,7 @@
+package s1.t1.n1.exercici1;
+
+class CordaInstrument extends Instrument {
+    CordaInstrument(String nom, float preu) {
+        super(Tipus.corda.toString(), nom, preu);
+    }
+}
--- src/s1/t1/n1/exercici1/Instrument.java
+++ src/s1/t1/n1/exercici1/Instrument.java
@@ -1,13 +1,6 @@
 package s1.t1.n1.exercici1;
 
 
-// If i use this interface it breaks the code. Why? 
-interface InstrumentDef{
-    void tocar();
-}
-
-
-
 class Instrument implements InstrumentDef {
     static {
         // Because it will only be loaded once, this will only be printed once. 
@@ -36,22 +29,5 @@ class Instrument implements InstrumentDef {
 
     public void tocar() {
         System.out.printf("Està sonant un instrument de %s. \n", tipus);
-    }
-}
-
-class VentInstrument extends Instrument {
-    VentInstrument(String nom, float preu) {
-        super(Tipus.vent.toString(), nom, preu);
-    }
-}
-
-class CordaInstrument extends Instrument {
-    CordaInstrument(String nom, float preu) {
-        super(Tipus.corda.toString(), nom, preu);
-    }
-}
-class PercusioInstrument extends Instrument {
-    PercusioInstrument(String nom, float preu) {
-        super(Tipus.percussió.toString(), nom, preu);
     }
 }
