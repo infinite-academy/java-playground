8066cfd281ca70c99030c736b96acb56ff93f3bc
emmaalecrim <emmaalecrim@gmail.com>
2024-05-14 10:54:26 +0200

    configured runtimes and wip ex1

--- src/s1/t4/app/src/main/java/t4/App.java
+++ src/s1/t4/app/src/main/java/t4/App.java
@@ -5,7 +5,7 @@ package t4;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World! T4";
     }
 
     public static void main(String[] args) {
--- src/s1/t4/app/src/test/java/t4/n1/exercici1/YearTest.java
+++ src/s1/t4/app/src/test/java/t4/n1/exercici1/YearTest.java
@@ -1,22 +1,24 @@
 package t4.n1.exercici1;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class YearTest {
     String[] months = new String[12];
     Year year = new Year(months);
 
-    @Test void yearHas12Months() {
+    @Test
+    void yearHas12Months() {
         assertTrue(year.months.length == 12);
     }
 
-    @Test void yearNotNull() {
+    @Test
+    void yearNotNull() {
         assertNotNull(year.months);
     }
 
-    @Test void yearHasAugust() {
+    @Test
+    void yearHasAugust() {
         assertTrue(year.months[7] == "August");
     }
 }
