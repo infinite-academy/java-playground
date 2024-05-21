79c0cd354b0ece6cd7e6b413fae339e37dc1c178
emmaalecrim <emmaalecrim@gmail.com>
2024-05-21 12:16:34 +0200

    include title print for better indent

--- src/s1/t5/n1/exercici2/DirectoryListerRecursive.java
+++ src/s1/t5/n1/exercici2/DirectoryListerRecursive.java
@@ -6,11 +6,12 @@ import java.io.File;
 import java.io.IOException;
 
 public class DirectoryListerRecursive extends DirectoryLister {
-    private static void printFileNames(File[] files, String indent) {
+    private static void printFileNames(File dir, File[] files, String indent) {
+        System.out.println("Contents of " + dir.getAbsolutePath() + ":");
         for (File file : files) {
             System.out.println(indent + (file.isDirectory() ? "D " : "F ") + file.getName());
             if (file.isDirectory()) {
-                printFileNames(file.listFiles(), indent + "  ");
+                printFileNames(dir, file.listFiles(), indent + "  ");
             }
         }
     }
@@ -20,9 +21,7 @@ public class DirectoryListerRecursive extends DirectoryLister {
         DirectoryLister.validateDirectory(dir);
 
         File[] files = DirectoryLister.getSortedFiles(dir);
-
-        System.out.println("Contents of " + dir.getAbsolutePath() + ":");
-        printFileNames(files, "");
+        printFileNames(dir, files, " ");
 
         for (File file : files) {
             if (file.isDirectory()) {
