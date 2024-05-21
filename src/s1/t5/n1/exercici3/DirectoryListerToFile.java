7dded6420f8436972500a514c28e089b2a6d4053
emmaalecrim <emmaalecrim@gmail.com>
2024-05-21 12:16:52 +0200

    exercici3 wip

--- src/s1/t5/n1/exercici3/DirectoryListerToFile.java
+++ src/s1/t5/n1/exercici3/DirectoryListerToFile.java
@@ -1,7 +1,9 @@
 package s1.t5.n1.exercici3;
 
+import s1.t5.n1.exercici1.DirectoryLister;
 import s1.t5.n1.exercici2.DirectoryListerRecursive;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -9,7 +11,32 @@ import java.nio.file.Paths;
 public class DirectoryListerToFile extends DirectoryListerRecursive {
     public static void listDirContentsRecursivelyToFile(String dirPath, String outputFilePath) throws IOException {
         StringBuilder output = new StringBuilder();
-        DirectoryListerRecursive.listDirContentsRecursively(dirPath);
+        listDirContentsRecursively(dirPath, output);
         Files.write(Paths.get(outputFilePath), output.toString().getBytes());
     }
+
+    public static void listDirContentsRecursively(String dirPath, StringBuilder output) throws IOException {
+        File dir = new File(dirPath);
+        DirectoryLister.validateDirectory(dir);
+
+        File[] files = DirectoryLister.getSortedFiles(dir);
+        appendContents(output, dir, files, " ");
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                listDirContentsRecursively(file.getAbsolutePath(), output);
+            }
+        }
+    }
+
+    public static void appendContents(StringBuilder output, File dir, File[] files, String indent) {
+        output.append("Contents of " + dir.getAbsolutePath() + ":" + "\n");
+        for (File file : files) {
+            output.append(indent + (file.isDirectory() ? "D " : "F ") + file.getName() + "\n");
+            if (file.isDirectory()) {
+                appendContents(output, dir, file.listFiles(), indent + "  ");
+            }
+        }
+    }
+
 }
