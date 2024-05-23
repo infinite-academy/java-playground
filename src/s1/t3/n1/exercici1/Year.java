9dfbde37eaf811101168f542b8793aec8e114a13
emmaalecrim <emmaalecrim@gmail.com>
2024-05-23 13:34:40 +0200

    publicly private

--- src/s1/t3/n1/exercici1/Year.java
+++ src/s1/t3/n1/exercici1/Year.java
@@ -3,43 +3,43 @@ package s1.t3.n1.exercici1;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-
- class Year {
-    // For the gregorian year we know this could be a simpler Array with 12 places. 
+public class Year {
+    // For the gregorian year we know this could be a simpler Array with 12 places.
     // ArrayList is used here for flexibility and to implement exercise logic.
-    // technicaly some of this logic could be reused(Year is modeled as collection of Months with Extra info. You could extend it to model a chinese year)
+    // technicaly some of this logic could be reused(Year is modeled as collection
+    // of Months with Extra info. You could extend it to model a chinese year)
     private final ArrayList<Month> months = new ArrayList<Month>(12);
     private int cardinal;
 
-    Year(){};
     Year(int cardinal) {
         this.cardinal = cardinal;
-        for(Month.MONTHS month : Month.MONTHS.values()){
-                this.months.add(new Month(month));
+        for (Month.MONTHS month : Month.MONTHS.values()) {
+            this.months.add(new Month(month));
         }
     }
-    
-    int getCardinal() {
+
+    public int getCardinal() {
         return cardinal;
     }
-    void setCardinal(int cardinal) {
+
+    public void setCardinal(int cardinal) {
         this.cardinal = cardinal;
     }
-    
-    void printCalendar(){
+
+    public void printCalendar() {
         System.out.println("Year calendar ---- " + cardinal + " ----");
-        for(Iterator<Month> i = this.months.iterator(); i.hasNext();){
+        for (Iterator<Month> i = this.months.iterator(); i.hasNext();) {
             Month month = i.next();
             System.out.println(month.name + " " + (month.ordinal + 1));
         }
         System.out.println("Size: " + months.size());
     }
 
-    void addMonth(int index, Month month){
+    public void addMonth(int index, Month month) {
         this.months.add(index, month);
     }
 
-    boolean addMonth(Month month){
+    public boolean addMonth(Month month) {
         return this.months.add(month);
     }
 }
