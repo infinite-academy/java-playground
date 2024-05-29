7fe7a001286cd81293139253fa76791c6107dfc3
Emma Alecrim <emmaalecrim@pm.me>
2024-05-29 09:50:54 +0000

    linted and added pool of countries so no dupes

--- src/s1/t3/n1/exercici2/Main.java
+++ src/s1/t3/n1/exercici2/Main.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class Main {
     void main(String[] args) {
-        List<Integer> list1 = new ArrayList<Integer>();
-        for(int i = 0; i < 10; i++) list1.add(i);
+        List<Integer> list1 = new ArrayList<>();
+        for (int i = 0; i < 10; i++)
+            list1.add(i);
         System.out.println(list1);
-        List<Integer> list2 = new ArrayList<Integer>();
+        List<Integer> list2 = new ArrayList<>();
         list2.addAll(list1.reversed());
         System.out.println(list2);
 
     }
 }
-
